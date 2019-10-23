9ca406f06d24eeca46fee2d2c1b1a7096782e251
kimchangkyu <kimchangkyu2@naver.com>
2019-10-23 16:42:58 +0900

    feat: fizzbuzz list comprehension

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-for i in range(1, 101):
-    if i % 15 == 0:
-        print('fizzbuzz')
-    elif i % 5 == 0:
-        print('buzz')
-    elif i % 3 == 0:
-        print('fizz')
-    else:
-        print(i)
-
-
+result = ["fizzbuzz" if i%15==0 else "fizz" if i%3==0 else "buzz" if i%==5 for i in range(1, 100+1)]
+print(result)
