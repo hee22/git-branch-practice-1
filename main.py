83d7ea8dc790fde228a5545dcc2d2ea90191401c
LeeJaeKeun14 <ljkean14@gmail.com>
2019-10-23 16:33:18 +0900

    fix: data
    fix

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-for i in range(1, 100 + 1):
-    if i % 15 == 0:
-        print("fizzbuzz")
-    elif i % 5 == 0:
-        print("buzz")
-    elif i % 3 == 0:
-        print("fizz")
-    else:
-        print(i)
+print('check')
+
